7fc0af645391166d1176fb600e63e3715352bdaa
N1Andrew <63069333+N1Andrew@users.noreply.github.com>
2020-09-23 19:18:38 -0400

    Add files via upload

--- MyFood/lib/main.dart
+++ MyFood/lib/main.dart
@@ -1,17 +1,27 @@
-=======
+import 'dart:ui';
+import './view/Login.dart';
 import 'package:flutter/material.dart';
-import './view/login.dart';
+import 'dart:async';
+import 'dart:io';
 
-void main() {
-  runApp(MyFood());
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  // This widget is the root of your application.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+  runApp(
+      MaterialApp(
+        theme: ThemeData.dark(),
+        home: MyLogin(title: 'MyAppLogin', camera: firstCamera,),
+      ),
+  );
 }
 
-class MyFood extends StatelessWidget {
-  @override 
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(),
-    );
-  }
-}
 
